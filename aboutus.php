297d6f655e3040caecfd492ff91aaf7365247e01
nekrochaa-n <37211768+nekrochaa-n@users.noreply.github.com>
2018-11-28 04:23:40 +0700

    tambah aboutus

---
+++ aboutus.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Konekin</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.bundle.min.js" integrity="sha384-pjaaA8dDz/5BgdFUPX6M/9SUZv4d12SUPF0axWc+VRZkx5xU3daN+lYb49+Ax+Tl" crossorigin="anonymous"></script>
+    
+    <link rel="stylesheet" href="custom.css">
+
+
+</head>
+<body>
+    
+
+    <div class="jumbotron">
+        <div class="container">
+            <h1 class="display-6">Konekin</h1>
+        </div>
+    </div>
+    
+    
+    <nav class="navbar navbar-expand-lg">
+        <div class="container">
+            <a href = "home.php" class="navbar-brand" href="#">Home</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample07" aria-controls="navbarsExample07" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+    
+            <div class="collapse navbar-collapse" id="navbarsExample07">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="https://example.com" id="dropdown07" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Faculties</a>
+                        <div class="dropdown-menu" aria-labelledby="dropdown07">
+                            <h1 class="dropdown-header">Faculties</h1>
+                            <a class="dropdown-item" href="#">FIA</a>
+                            <a class="dropdown-item" href="#">FTI</a>
+                            <a class="dropdown-item" href="#">FTE</a>
+                            <a class="dropdown-item" href="#">FTSLK</a>
+                            <a class="dropdown-item" href="#">FADP</a>
+                            <a class="dropdown-item" href="#">FTK</a>
+                            <a class="dropdown-item" href="#">FMKSD</a>
+                            <a class="dropdown-item" href="#">FTIK</a>
+                            <a class="dropdown-item" href="#">FBMT</a>
+                            <a class="dropdown-item" href="#">FV</a>
+                        </div>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">About</a>
+                </li>
+            </ul>
+            <form class="form-inline my-2 my-md-0">
+                <input class="form-control" type="text" placeholder="Search" aria-label="Search">
+            </form>
+            </div>
+        </div>
+    </nav>
+
+
+
+
+                
+            <div class="col-sm-10">
+                <h1>About Us</h1>
+                <p>Konekin is Social Media Platform Exclusive for student of Instute Teknologi Sepuluh Nopember. Konekin have two main feature, there are : </p>
+                <h3>1. Post home</h3>
+                <p>User can post whatever he want in here. this post will remain unless administator found it violate EULA.</p>
+                <h3>2. Follow Faculty</h3>
+                <p>User can follow faculty. After follow one or more Faculty, user will be informed about event that happen at that faculty.</p>
+            </div>
+        </div>
+
+        
+
+    </div>
+
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</body>
+</html>
